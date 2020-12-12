1dd6fe2818fe9c50054b1f07e944294dc22d956b
gieldemeulemeester <31191620+gieldemeulemeester@users.noreply.github.com>
2020-12-12 20:52:50 +0100

    Code cleanup

--- export_safari_reading_list.py
+++ export_safari_reading_list.py
@@ -2,14 +2,38 @@
 
 import sys, os, argparse, plistlib, webloc
 
-def write_weblocs(plistDict, args):
+def get_bookmarks_dict(bookmarksFile: str) -> dict:
+    if bookmarksFile == None:
+        if sys.stdin.isatty():
+            print("the following arguments are required: -i/--ifile")
+            exit(1)
+        return plistlib.loads(sys.stdin.buffer.read())
+    
+    try:
+        with open(bookmarksFile, 'rb') as f:
+            return plistlib.load(f)
+    except FileNotFoundError:
+        print(f"the bookmarks file '{bookmarksFile}' was not found")
+        exit(1)
+    except PermissionError:
+        print(f"the bookmarks file '{bookmarksFile}' is not accessible")
+        exit(1)
+
+def sanitize_output_dir(path: str) -> str:
+    if path == None:
+        path = os.getcwd()
+    elif not os.path.isdir(path):
+        os.mkdir(path)
+    return path.rstrip('/')
+
+def write_weblocs(bookmarksDict: dict, args) -> int:
     childCount = 0
-    if 'Children' in plistDict.keys():
-        for child in plistDict['Children']:
+    if 'Children' in bookmarksDict.keys():
+        for child in bookmarksDict['Children']:
             childCount += write_weblocs(child, args)
-    if 'ReadingList' in plistDict.keys():
-        filename = f"{plistDict['URIDictionary']['title'].replace('/', '-')}.webloc"
-        webloc.write(f"{args.opath}/{filename}", plistDict['URLString'])
+    if 'ReadingList' in bookmarksDict.keys():
+        filename = f"{bookmarksDict['URIDictionary']['title'].replace('/', '-')}.webloc"
+        webloc.write(f"{args.opath}/{filename}", bookmarksDict['URLString'])
         childCount += 1
         if args.verbose: print(filename)
     return childCount
@@ -21,27 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
-    if args.bookmarks == None:
-        if sys.stdin.isatty():
-            print("the following arguments are required: -i/--ifile")
-            exit(1)
-        
-        rlist = plistlib.loads(sys.stdin.buffer.read())    
-    else:
-        try:
-            with open(args.bookmarks, 'rb') as f:
-                rlist = plistlib.load(f)
-        except FileNotFoundError:
-            print(f"the bookmarks file '{args.bookmarks}' was not found")
-            exit(1)
-        except PermissionError:
-            print(f"the bookmarks file '{args.bookmarks}' is not accessible")
-            exit(1)        
-
-    if args.opath == None:
-        args.opath = os.getcwd()
-    elif not os.path.isdir(args.opath):
-        os.mkdir(args.opath)    
-    args.opath.rstrip('/')
-    
-    print("{} weblocs created".format(write_weblocs(rlist, args)))
+    bookmarksDict = get_bookmarks_dict(args.bookmarks)
+    args.opath = sanitize_output_dir(args.opath)
+    count = write_weblocs(bookmarksDict, args)
+    print(f"{count} weblocs exported")
